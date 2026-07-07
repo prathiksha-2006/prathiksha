a6967358347a2413d90598fbe6edfbf1c0d6451d
prathiksha-2006 <prathikshan27@gmail.com>
2026-07-07 11:48:55 +0530

    first commit

---
+++ operators.py
@@ -0,0 +1,15 @@
+product_charges = 1000
+delivery_charges = 200
+total = product_charges + delivery_charges
+print(total)
+
+a = 3
+b = 9
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
