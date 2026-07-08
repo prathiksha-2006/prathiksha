12c82c1e6a2574d1cc7d549fcdbc0072968dfdce
prathiksha-2006 <prathikshan27@gmail.com>
2026-07-08 09:42:28 +0530

    new commit

---
+++ Day.3.py
@@ -0,0 +1,13 @@
+# student = ("ram","sam","bheem")
+# print(student[-2])
+# #########tuple is a collection used to store multiple
+# numbers = (10,20,30,40)
+# print(numbers [-3])
+
+# data = (1,2,3)
+# data[0] = 100
+# print(data)
+
+x = (1,2,3,2,1,1,4)
+print(x.count(1))
+print(x.count(2))
