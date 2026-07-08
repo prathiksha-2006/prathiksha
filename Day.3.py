0d4f5df2383d948413328b5c45c92d334ddf17b7
prathiksha-2006 <prathikshan27@gmail.com>
2026-07-08 15:18:23 +0530

    new commit

--- Day.3.py
+++ Day.3.py
@@ -8,6 +8,138 @@
 # data[0] = 100
 # print(data)
 
-x = (1,2,3,2,1,1,4)
-print(x.count(1))
-print(x.count(2))
+# x = (1,2,3,2,1,1,4)
+# print(x.count(1))
+# print(x.count(2))
+
+# x = ("apple","banana","grapes","banana")
+# print(x.count("banana"))
+
+# num = (10,20,30,40,50)
+# print(num[1:4])
+
+# ######sets###
+# x = {1,2,3,1,1,1,4}
+# print(x)
+# data = {1,2,3}
+# data.add(4)
+# print(data)
+
+# a ={1,2,3}
+# b = {3,4,5}
+# print (a|b)
+
+# ####function#####
+# def greeting():
+#     print("hello students")
+# greeting()
+
+# def add():
+#     return 10 + 20
+# result = add()
+# print(result)
+
+# def subtraction():
+#     return 20 - 5
+# result = subtraction()
+# print(result)
+
+# def add(a,b):
+#     print(a+b)
+# add(10,20)
+
+# def add(*numbers):
+#     print(numbers)
+# add(10,20,30,40,50,60)
+
+# def darling(*numbers):
+#     print(numbers)
+# darling(10,20,30,40,50,60)
+
+# def add(*num):
+#     total = 8
+#     for i in num:
+#         total += i
+#     print(total)
+# add(10,20,30,40,50,60)
+
+# def student(**details):
+#     print("name :",details["name"])
+#     print("age :",details["age"])
+#     print("job :",details["job"])
+# student(
+#     name="poo",
+#     age=20,
+#     job="sales",
+# )
+
+# def square(x):
+#     return x * x
+# print(square(16))
+
+# square = lambda x:x*x*x
+# print(square(25))
+
+# add = lambda a,b:a+b
+# print(add(10,20))
+
+# even_odd = lambda x:"even" if x%2 else "odd"
+# print(even_odd(10))
+# print(even_odd(7))
+
+# upper = lambda x : x.upper()
+# print(upper("lucky"))
+
+# prathi = lambda text:len(text)
+# print(prathi("rukmini"))
+
+# file = open("student.txt","w")
+# file.write("hello lucky")
+# file.close()
+
+# print("data written successfully")
+# file= open("student.txt","r")
+# data=file.read()
+# print(data)
+# file.close()
+
+# file=open("student.txt","a")
+# file.write("\nhow are you")
+# file.close()
+
+# print("data append successfully")
+
+# file=open("student.txt","r")
+# print(file.read())
+# file.close()
+
+
+# try:
+#     num = int(input("enter number:"))
+#     print(num)
+# except ValueError:
+#     print("only number allowed")
+
+# try:
+#     a = int(input("enter A:"))
+#     b = int(input("enter b:"))
+#     print(a/b)
+# except ZeroDivisionError:
+#     print("cannot divided by zero")
+# except ValueError:
+#     print("enter onnly numbers")
+
+# try:
+#     file =open("data.txt")
+#     print(file.read())
+# except:
+#     print("file error")
+# finally:
+#     print("program completed")
+
+# try:
+#     print(10/2)
+# except:
+#     print("error")
+# else:
+#     print("success")
--- operators.py
+++ operators.py
@@ -1,21 +1,21 @@
 product_charges = 1000
- delivery_charges = 200
- total = product_charges + delivery_charges
- print(total)
+delivery_charges = 200
+total = product_charges + delivery_charges
+print(total)
 
 
 
  #operators
- a = 3
- b = 9
+a = 3
+b = 9
 
- print(a+b)
- print(a-b)
- print(a*b)
- print(a/b)
- print(a//b)
- print(a%b)
- print(a**b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
 
 
 
@@ -141,10 +141,9 @@ for i in range(2,7):
      print("message sent to",users)
 
  name = "dhoni"
-# for ch in name:
-#     print(ch)
-
-count =1
+ for ch in name:
+    print(ch)
+    count =1
  while count<=5:
          print(count)
     count +=1
